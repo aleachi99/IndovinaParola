bd4eb8a29aae842f7383f56c27a66db19e73ebc1
rossi.alessandroachi <rossi.alessandroachi@TAVE11.OSDBSESTO.local>
2017-12-15 13:34:40 +0100

    Inzio implementazione algoritmo client-server lato server

--- IndovinaParolaServer/src/indovinaparolaserver/Partita.java
+++ IndovinaParolaServer/src/indovinaparolaserver/Partita.java
@@ -16,21 +16,39 @@ import java.util.logging.Logger;
  */
 public class Partita extends Thread {
     private Socket socketClient;
+    private PrintStream outputClient;
+    private BufferedReader inputClient;
+    private String nomeGiocatore;
+    private Parola parola;
     public Partita(Socket s){
         socketClient=s;
+        parola=new Parola();
     }
     @Override
     public void run(){
         try {
-            PrintStream outputClient=new PrintStream(socketClient.getOutputStream());
+            outputClient=new PrintStream(socketClient.getOutputStream());
         } catch (IOException ex) {
             Logger.getLogger(Partita.class.getName()).log(Level.SEVERE, null, ex);
         }
         try {
-            BufferedReader inputClient=new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+            inputClient=new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
         } catch (IOException ex) {
             Logger.getLogger(Partita.class.getName()).log(Level.SEVERE, null, ex);
         }
+        outputClient.println("Giocatore: ");
+        String inClient="";
+        do{
+            try {
+                inClient = inputClient.readLine();
+            } catch (IOException ex) {
+                Logger.getLogger(Partita.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }while(!inClient.startsWith("NomeGiocatore: "));
+        nomeGiocatore=inClient.substring(15);
+        do{
+            //Fai la partita lato server;
+        }while(!parola.isParolaIndovinata());
     }
     
     
