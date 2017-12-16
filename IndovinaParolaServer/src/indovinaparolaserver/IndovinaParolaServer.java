1f8462f2b2f2ce0a3c75724ac0448f8110d72663
AlessandroAchille <AlessandroAchille@10.1.100.3>
2017-12-16 14:56:42 +0100

    Implementato avvio del programma

--- IndovinaParolaServer/src/indovinaparolaserver/IndovinaParolaServer.java
+++ IndovinaParolaServer/src/indovinaparolaserver/IndovinaParolaServer.java
@@ -4,8 +4,11 @@
  * and open the template in the editor.
  */
 package indovinaparolaserver;
+import java.io.IOException;
 import java.net.*;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author rossi.alessandroachi
@@ -15,6 +18,12 @@ public class IndovinaParolaServer {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {    
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(5000);
+            Partita partita = new Partita(server.accept());
+        } catch (IOException ex) {
+            Logger.getLogger(IndovinaParolaServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
