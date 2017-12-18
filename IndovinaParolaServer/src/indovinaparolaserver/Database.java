8a5200063359ee48c770fbee454ac00caf9487c6
rossi.alessandroachi <rossi.alessandroachi@TAVE11.OSDBSESTO.local>
2017-12-18 12:02:11 +0100

    Inizio implementazione database

---
+++ IndovinaParolaServer/src/indovinaparolaserver/Database.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package indovinaparolaserver;
+
+/**
+ *
+ * @author rossi.alessandroachi
+ */
+import java.sql.*;
+public class Database {
+    public Database(){
+        try{
+            Class.forName("a");
+        }catch (Exception exc){
+            
+        }
+    }
+}
