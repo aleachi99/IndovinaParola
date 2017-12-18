120949d20fd9e4fdd156066709f353a9b3b4e060
AlessandroAchille <AlessandroAchille@10.1.100.3>
2017-12-18 23:04:54 +0100

    Creata classe Database. Con questa classe fornisce i metodi per ottenere una nuova parola dal dizionario, oppure per salvare una partita con nome giocatore e parola estratta sul database. é inoltre possibile chiudere la connnessione con il metodo "close"

--- IndovinaParolaServer/src/indovinaparolaserver/Database.java
+++ IndovinaParolaServer/src/indovinaparolaserver/Database.java
@@ -10,12 +10,54 @@ package indovinaparolaserver;
  * @author rossi.alessandroachi
  */
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class Database {
+    private Connection con;
+    
     public Database(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            // Setup the connection with the DB
+            con = DriverManager
+                    .getConnection("jdbc:mysql://10.1.100.8/tecnologia?"
+                            + "user=tecnologia&password=password");
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public String getParola(){
         try{
-            Class.forName("a");
+            Statement stmt = con.createStatement();
+            ResultSet rs=stmt.executeQuery("SELECT COUNT(*) AS numeroRighe FROM dizionario");
+            rs.next();
+            int numeroParole=rs.getInt("numeroRighe");
+            numeroParole=(int)((Math.random()*1000)%numeroParole)+1;
+            rs=stmt.executeQuery("SELECT parola FROM dizionario WHERE id="+numeroParole);
+            rs.next();
+            String parolaScelta=rs.getString("parola");
+            System.out.println("Parola Scelta:" + parolaScelta);
+            return parolaScelta;
         }catch (Exception exc){
-            
+            System.out.println(exc);
+            exc.printStackTrace();
+            return "";
+        }
+    }
+    public void closeConnection() throws SQLException{
+        con.close();
+    }
+    public void inserisciPartita(String nomeGiocatore, String parola){
+        Statement stmt;
+        try {
+            stmt = con.createStatement();
+            stmt.executeUpdate("INSERT INTO partita (nomeGiocatore, parola) VALUES (\""+nomeGiocatore+"\",\""+parola+"\");");
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
